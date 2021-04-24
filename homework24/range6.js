d59566c0ee426b2944423901dcb97eb26b64d6f2
ПК\Nastya <nassjko7@gmail.com>
2021-04-24 10:53:38 +0300

    добавила задачу 6 из 10ти

--- homework24/range6.js
+++ homework24/range6.js
@@ -1,6 +1,28 @@
 //Зациклить калькулятор. Запросить у пользователя 2 числа и знак, решить пример, вывести результат и спросить, хочет ли он решить еще один пример. 
 //И так до тех пор, пока пользователь не откажется.
 
-let a = +prompt('введите первое число');
-let b = +prompt('введите второе число');
-let sign = +prompt('введите знак');
+let a;
+let b;
+let sign;
+let moreCalculation = true;
+
+function calculate() {
+    a = +prompt('введите первое число');
+    b = +prompt('введите второе число');
+    sign = prompt('введите знак');
+
+    if (sign === "+") {
+        alert(a + b);
+    } else if (sign === "-") {
+        alert(a - b);
+    } else if (sign === "*") {
+        alert(a * b);
+    } else if (sign === "/") {
+        alert(a / b);
+    }
+}
+
+while (moreCalculation) {
+    calculate();
+    moreCalculation = confirm('хочешь  решить еще один пример?');
+}
